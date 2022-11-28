1f62f4ef8a32391a5bfacb326920e4d9c9ffd8c6
Haris Volos <hvolos01@ucy.ac.cy>
2022-11-28 16:32:25 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,27 @@ sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jre-headless scala openssh-server openssh-client syslinux-utils python3-pip socat;
 
+
+# Setup password-less ssh between nodes
+for user in $USERS; do
+    if [ "$user" = "root" ]; then
+        ssh_dir=/root/.ssh
+    else
+        ssh_dir=/users/$user/.ssh
+    fi
+    /usr/bin/geni-get key > $ssh_dir/id_rsa
+    chmod 600 $ssh_dir/id_rsa
+    chown $user: $ssh_dir/id_rsa
+    ssh-keygen -y -f $ssh_dir/id_rsa > $ssh_dir/id_rsa.pub
+    cat $ssh_dir/id_rsa.pub >> $ssh_dir/authorized_keys2
+    chmod 644 $ssh_dir/authorized_keys2
+    cat >>$ssh_dir/config <<EOL
+    Host *
+         StrictHostKeyChecking no
+EOL
+    chmod 644 $ssh_dir/config
+done
+
 awk 'NR>1 {print $NF}' /etc/hosts | grep -v 'master' > /local/host_list
 # docker
 sudo apt-get install -y \
